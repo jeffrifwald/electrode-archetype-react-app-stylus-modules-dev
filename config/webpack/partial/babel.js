c485752d5b1f3b063849d619087446aa07c77359
Jeff McRiffey <jmcriffey@gmail.com>
2017-02-13 14:33:45 -0500

    rename config

--- config/webpack/partial/babel.js
+++ config/webpack/partial/babel.js
@@ -25,7 +25,7 @@ module.exports = function (babel) {
               "babel-loader"
             ].filter(_.identity),
             query: babel
-          }, archetype.webpack.babelLoader),
+          }, archetype.webpack.extendBabelLoader),
           {
             name: "json",
             test: /\.json$/,
